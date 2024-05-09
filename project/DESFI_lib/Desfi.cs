9a7c4a09150363e527c226563c7add904469521c
ivnktrv <ivanlein777@gmail.com>
2024-05-09 17:41:55 +0300

    first commit :D

---
+++ project/DESFI_lib/Desfi.cs
@@ -0,0 +1,95 @@
+﻿using System.Globalization;
+
+namespace DESFI_lib;
+
+public class Desfi
+{
+    private const char NULL_BYTE = '\x00';
+
+    public void destroy(string file, bool writeNullBytes = false, bool passes = false,
+        bool randBytes = false, bool pasOneRandByte = false, bool dod = false)
+    {
+        try
+        {
+            using BinaryWriter br = new(File.Open(file, FileMode.Open));
+
+            if (writeNullBytes)
+            {
+                Console.WriteLine("[i] Запись NULL байтов");
+                while (br.BaseStream.Position != br.BaseStream.Length)
+                {
+                    br.Write(NULL_BYTE);
+                }
+                Console.WriteLine("[+] Сделано");
+                br.Close();
+                File.Delete(file);
+                Console.WriteLine($"\n[+] Файл удалён: {file}");
+            }
+            if (passes)
+            {
+                Console.Write("Сколько будет проходов?: ");
+                int countPasses = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                if (!randBytes)
+                {
+                    for (int i = 0; i < countPasses; i++)
+                    {
+                        Console.Write($"Введите HEX значение для {i + 1} прохода: ");
+                        int getHex = int.Parse(Console.ReadLine(), NumberStyles.HexNumber);
+                        while (br.BaseStream.Position != br.BaseStream.Length)
+                        {
+                            br.Write(getHex);
+                        }
+                        br.BaseStream.Position = 0;
+                    }
+                }
+                else if (randBytes)
+                {
+                    for (int i = 0; i < countPasses; i++)
+                    {
+                        if (!pasOneRandByte)
+                        {
+                            while (br.BaseStream.Position != br.BaseStream.Length)
+                            {
+                                br.Write(new Random().Next(0, 255));
+                            }
+                        }
+                        else
+                        {
+                            int rChar = new Random().Next(0, 255);
+                            while (br.BaseStream.Position != br.BaseStream.Length)
+                            {
+                                br.Write(rChar);
+                            }
+                        }
+                        br.BaseStream.Position = 0;
+                        Console.WriteLine($"[i] Проход {i + 1} завершён");
+                    }
+                    Console.Write("\n[?] Перезаписать содержимое NULL байтами? [y/n]: ");
+                    ConsoleKeyInfo key = Console.ReadKey();
+
+                    if (key.Key == ConsoleKey.Y || key.Key == ConsoleKey.Enter)
+                    {
+                        while (br.BaseStream.Position != br.BaseStream.Length)
+                        {
+                            br.Write(NULL_BYTE);
+                        }
+                        Console.WriteLine("\n[i] Добавление NULL байтов завершено");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\n[-] Отмена");
+                    }
+                }
+                br.Close();
+                File.Delete(file);
+                Console.WriteLine($"\n[+] Файл удалён: {file}");
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("[-] Файл не найден");
+        }
+    }
+}
---
+++ project/desfi/Program.cs
@@ -0,0 +1,114 @@
+﻿using DESFI_lib;
+
+namespace desfi;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        
+        printLogo();
+
+        Console.Write("Укажите файл: ");
+        string? file = Console.ReadLine();
+
+        Desfi d = new();
+        
+        Console.Write("""
+
+            ### ЧТО ЗАПИСАТЬ В ФАЙЛ ###
+
+            0 - Записать NULL байты
+            1 - Выбрать образец прохода
+            2 - Записать рандомные байты
+            3 - Для каждого прохода выбрать рандомный байт
+            
+            -> 
+            """);
+        
+        ConsoleKeyInfo key = Console.ReadKey();
+
+        Console.WriteLine("\n");
+        if (key.Key == ConsoleKey.NumPad0 || key.Key == ConsoleKey.D0)
+        {
+            d.destroy(file, writeNullBytes: true);
+        }
+        else if (key.Key == ConsoleKey.NumPad1 || key.Key == ConsoleKey.D1)
+        {
+            d.destroy(file, passes: true, randBytes: false);
+        }
+        else if (key.Key == ConsoleKey.NumPad2 || key.Key == ConsoleKey.D2)
+        {
+            d.destroy(file, passes: true, randBytes: true);
+        }
+        else if (key.Key == ConsoleKey.NumPad3 || key.Key == ConsoleKey.D3)
+        {
+            d.destroy(file, passes: true, randBytes: true, pasOneRandByte: true);
+        }
+        else if (key.Key == ConsoleKey.H)
+        {
+            Console.WriteLine("""
+                ### ПРИНЦИП РАБОТЫ МЕТОДОВ УДАЛЕНИЯ ###
+
+                file.txt
+                ----------------------------
+                HELLO WORLD
+
+                ______________________________________________________
+                |0:                                                  |
+                |       - ЗАПИСЫВАЮТСЯ NULL БАЙТЫ                    |
+                |                                                    |
+                |       file.txt                                     |
+                |       ----------------------------                 |
+                |       00 00 00 00 00 00 00 00 00 00                |
+                |       00                                           |
+                |____________________________________________________|
+                |1:                                                  |
+                |       - УКАЗЫВАЕТЕ КОЛ-ВО ПРОХОДОВ                 |
+                |                                                    |
+                |       ЦИКЛ:                                        |
+                |           - ВЫБИРАЕТЕ HEX ЗНАЧЕНИЕ (НАПРИМЕР AB)   |
+                |                                                    |
+                |           file.txt                                 |
+                |           ----------------------------             |
+                |           AB AB AB AB AB AB AB AB AB AB            |
+                |           AB                                       |
+                |____________________________________________________|
+                |2:                                                  |
+                |    ЦИКЛ:                                           |
+                |        - ЗАПИСЫВАЮТСЯ РАНДОМНЫЕ БАЙТЫ              |
+                |                                                    |
+                |       file.txt                                     |
+                |       ----------------------------                 |
+                |       10 AB FF AA CA 1E D5 F0 1D 21                |
+                |       15                                           |
+                |____________________________________________________|
+                |3:                                                  |
+                |   - ВЫБИРАЕТСЯ РАНДОМНЫЙ БАЙТ (НАПРИМЕР FF)        |
+                |                                                    |
+                |   ЦИКЛ:                                            |
+                |                                                    |
+                |       file.txt                                     |
+                |       ----------------------------                 |
+                |       FF FF FF FF FF FF FF FF FF FF                |
+                |       FF                                           |
+                |____________________________________________________|
+                """);
+            Console.ReadKey();
+        }
+        Console.ReadKey();
+    }
+    
+    private static void printLogo()
+    {
+        Console.WriteLine("""
+                _____     _____   ____   ____   ____   *
+                |    \    |    \ /  . \ / __|  / ___| ___  DESTROY
+              __|____|__  |  | | |  __/ \__ \  | |__  | |  THE FILE
+              |________|  |  | | | |       \ \ |  __| | |  FOREVER
+                 |  /|    |    | | |__  ___/ / | |    | |
+                |/ |  \   |____/ \____| |___/  |_|    |_|  v1.0
+              
+            """);
+    }
+}
